8803968897b51f4eb366281feab28cb6e8026cce
quchen <1987quchen@gmail.com>
2012-09-10 20:51:54 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LayoutPractice::Application.config.secret_token = '6dd55d4b57df33255f6756d5725e6fee8286e1486896e531825538630f4667787dddafd66c89e0abc0040f205e3ec448c0cae57bec5658ded7595cfa59dcd097'
